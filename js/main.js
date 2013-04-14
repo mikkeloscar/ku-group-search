2e1cee75add4e6ee92d8ac05e4301bb897e55e62
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2013-04-14 03:27:43 +0200

    Removed debug loggin from main.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 
-console.log("TEST");
-
 var MyCategoryCrawler = new CategoryCrawler({
     url: "https://groups.ku.dk/Sider/category.aspx",
     pattern: "https://groups.ku.dk:443/Sider/categoryresults.aspx"
